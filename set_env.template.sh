655c46583ce004a705a438e609ec81c4201752dc
Darren Seet <darren@seet.dev>
2020-07-02 01:58:33 -0400

    feat: Complete Docker configuration and setup code

---
+++ set_env.template.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_DB=
+export AWS_BUCKET=
+export AWS_REGION=
+export AWS_PROFILE=
+export JWT_SECRET=
+export DOCKER_USERNAME=
+export DOCKER_PASSWORD=
+export FRONTEND_URL=http://localhost:8100
--- udagram-feed-service/src/migrations/20190325-create-feed-item.js
+++ udagram-feed-service/src/migrations/20190325-create-feed-item.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('FeedItem', {
+    return queryInterface.createTable("FeedItem", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -24,7 +24,7 @@ module.exports = {
       },
     });
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('FeedItem');
+  down: (queryInterface) => {
+    return queryInterface.dropTable("FeedItem");
   },
 };
